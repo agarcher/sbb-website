6ef80ddea517970784501f9b48627287967aa664
Adam Archer <agarcher@gmail.com>
2024-11-15 14:38:39 -0500

    add company copywrite and support page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "press-start": ['"Press Start 2P"', "cursive"],
+        "press-start": ['"Press Start 2P"', "sans-serif"],
       },
       listStyleType: {
         square: 'square',
